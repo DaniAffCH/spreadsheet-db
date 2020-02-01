d5c938f5ef1b3605c05735b6b1de34db6d93948c
DaniAffCH <danieleaffinita2000@gmail.com>
2020-02-01 12:35:13 +0100

    minor bug fixed, completed createFields

--- spreadsheet-db/models.py
+++ spreadsheet-db/models.py
@@ -35,15 +35,18 @@ class DB:
     def createTable(self, title, rows=1000, cols=1000):
         self.ws = self.sheet.add_worksheet(title=title, rows=rows, cols=cols)
 
-    def createFields(self, *nameValue, overwrite = False):
+    def createFields(self, nameValue, overwrite = False):
+        if type(nameValue) != list:
+            raise Exception("Expected list type, given {}".format(type(nameValue).__name__))
+
         for e in nameValue:
             if type(e) != tuple:
-                raise Exception("Expected tuple type, given {}".format(type(e).__name__))
+                raise Exception("Expected list of tuple, given {}".format(type(e).__name__))
 
         # controllo se la prima riga sia occupata da A alla lunghezza di nameValue
         actualFields = [obj.value for obj in self.ws.range('A1:{}1'.format(chr(64+len(nameValue))))]
         if actualFields.count("") != len(nameValue) and not overwrite :
             raise Exception("Table has already fields values")
-        elif overwrite:
-            # TO DO
-            pass
+        for n,element in enumerate(nameValue):
+            self.ws.update_acell('{}1'.format(chr(65+n)), element[0])
+            self.ws.update_acell('{}2'.format(chr(65+n)), element[1])
